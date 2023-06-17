339862bc0cee57731914b2504824fc3ea8db1294
Venkatesh Raju <venkatesh.raju20@gmail.com>
2023-06-17 21:56:33 +0530

    added pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
+  const fetchProducts = () => {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data.products));
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+  const handlePage = (selectedPage: number) => {
+    if (
+      selectedPage >= 1 &&
+      selectedPage <= products.length / 10 &&
+      selectedPage !== page
+    ) {
+      setPage(selectedPage);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {products.length ? (
+        <div className="products">
+          {products.slice(page * 10 - 10, page * 10).map((product: any) => (
+            <div key={product.id} className="products__single">
+              <img src={product.thumbnail} alt={product.title} />
+              <span>{product.title}</span>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="loader">
+          <div>Loading Products...</div>
+        </div>
+      )}
+      {products.length && (
+        <div className="pagination">
+          <span
+            className={page > 1 ? "" : "pagination__disable"}
+            onClick={() => handlePage(page - 1)}
+          >
+            Previous
+          </span>
+          {[...Array(products.length / 10)].map((_, i) => {
+            return (
+              <span
+                key={i}
+                className={page === i + 1 ? "pagination__selected" : ""}
+                onClick={() => handlePage(i + 1)}
+              >
+                {i + 1}
+              </span>
+            );
+          })}
+          <span
+            className={page < products.length / 10 ? "" : "pagination__disable"}
+            onClick={() => handlePage(page + 1)}
+          >
+            Next
+          </span>
+        </div>
+      )}
+    </>
   );
 }
 
